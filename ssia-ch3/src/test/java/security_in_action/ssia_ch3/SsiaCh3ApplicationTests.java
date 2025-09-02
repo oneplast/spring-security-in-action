4c7d1e2b929088ef67ccbf8711f13bbfbe76e08f
Gyeonghun Min <riverpower6@naver.com>
2025-09-02 16:01:02 +0900

    [SETTING] Chapter3 프로젝트 세팅

---
+++ ssia-ch3/src/test/java/security_in_action/ssia_ch3/SsiaCh3ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
