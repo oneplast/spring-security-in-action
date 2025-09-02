8610f6841783b6562ca3a26a0676f850a76b2bde
Gyeonghun Min <riverpower6@naver.com>
2025-09-02 14:03:17 +0900

    [FEAT] SecurityFilterChain 빈 생성 및 httpBasic, permitAll 적용
    [DESCRIPTION] 2.3.2 실습 내용

--- ssia-ch2/src/main/java/security_in_action/ssia_ch2/config/ProjectConfig.java
+++ ssia-ch2/src/main/java/security_in_action/ssia_ch2/config/ProjectConfig.java
@@ -2,14 +2,28 @@ package security_in_action.ssia_ch2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
