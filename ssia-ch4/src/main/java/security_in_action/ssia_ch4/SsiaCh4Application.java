021e1c4adf82976d5b6ffb0ce0c6280fac8125f2
Gyeonghun Min <riverpower6@naver.com>
2025-09-02 18:19:25 +0900

    [SETTING] Chapter4 프로젝트 세팅

---
+++ ssia-ch4/src/main/java/security_in_action/ssia_ch4/SsiaCh4Application.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh4Application.class, args);
+	}
+
+}
---
+++ ssia-ch4/src/test/java/security_in_action/ssia_ch4/SsiaCh4ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
