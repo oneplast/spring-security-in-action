905ec327242bc228d640c9f018ae150900b7cd74
Gyeonghun Min <riverpower6@naver.com>
2025-09-02 18:10:31 +0900

    [REFACTOR] JdbcUserDetailsManager 쿼리 지정
    [DESCRIPTION]
    JdbcUserDetailsManager 에서 실행시킬 User, Authority 찾는 쿼리 직접 등록

--- ssia-ch3/src/main/java/security_in_action/ssia_ch3/config/ProjectConfig.java
+++ ssia-ch3/src/main/java/security_in_action/ssia_ch3/config/ProjectConfig.java
@@ -17,10 +17,23 @@ public class ProjectConfig {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from users where username = ?";
+        String authsByUserQuery = "select username, authority from authorities where username = ?";
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+    }
+/*
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
+*/
 
-//    @Bean
+    //    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails u = new User("john", "12345", "read");
         List<UserDetails> users = List.of(u);
