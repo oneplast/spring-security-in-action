d0069ab2e622231e73b77a84dcf65b296ea921b4
Gyeonghun Min <riverpower6@naver.com>
2025-09-03 19:18:11 +0900

    [FEAT] HelloController에서 Authentication 사용
    [DESCRIPTION]
    1. HelloController에서 Authentication 사용
      - HandlerMethodArgumentResolver 중 AuthenticationPrincipalArgumentResovler가 작동
      - 객체 바인딩 시, SecurityContextHolder.getContext().getAuthentication() (+.getPrincipal()) 사용하여 바인딩

--- ssia-ch5/src/main/java/security_in_action/ssia_ch5/controller/HelloController.java
+++ ssia-ch5/src/main/java/security_in_action/ssia_ch5/controller/HelloController.java
@@ -1,5 +1,6 @@
 package security_in_action.ssia_ch5.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public String hello(Authentication a) {
+        // MethodArgumentResolver 중 AuthenticationPrincipalArgumentResolver 작동
+        // 여기서 객체 바인딩 할 때, SecurityContextHolder.getContext().getAuthentication() (+.getPrincipal()) 사용
+
+        return "Hello, " + a.getName() + "!";
     }
 }
