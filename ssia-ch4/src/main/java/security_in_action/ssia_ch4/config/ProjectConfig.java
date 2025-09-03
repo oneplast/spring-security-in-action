5c90cac6bf075601fd93227b800b5eb9fff18581
Gyeonghun Min <riverpower6@naver.com>
2025-09-03 13:27:19 +0900

    [FEAT] DelegatingPasswordEncoder 구현

---
+++ ssia-ch4/src/main/java/security_in_action/ssia_ch4/config/ProjectConfig.java
@@ -0,0 +1,24 @@
+package security_in_action.ssia_ch4.config;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    }
+}
