eac8bb9033a350e7e7c30caa3c99d13bca3dfdd3
Gyeonghun Min <riverpower6@naver.com>
2025-09-04 14:51:22 +0900

    [FEAT] DelegatingSecurityContextCallable 적용
    [DESCRIPTION]
    1. 현재 스레드의 Authentication 정보를 얻어오는 Callable인 task 작성
      - 유저의 이름을 반환
    2. Executors.newCachedThreadPool()로 새로운 스레드풀 생성
    3. task를 DelegatingSecurityContextCallable로 감싼 contextTask 생성
      - 해당 callable이 호출되는 시점의 SecurityContext 상태를 캡쳐
      - 새 스레드에서 실행될 때, 캡쳐한 SecurityContext를 새 스레드의 ThreadLocal에 저장
      - callable 작업이 끝나도 기존 스레드의 SecurityContext는 유지
      - SecurityContextHolder의 MODE_INHERITABLETHREADLOCAL의 역할을 대신 수행 (모드 설정 필요 없음)
    4. submit(contextTask).get()으로 작업 수행
      - 스레드풀에서 스레드를 이 때 가져오며, 스레드풀에 스레드가 없으면 새로운 스레드 생성

--- ssia-ch5/src/main/java/security_in_action/ssia_ch5/config/ProjectConfig.java
+++ ssia-ch5/src/main/java/security_in_action/ssia_ch5/config/ProjectConfig.java
@@ -2,14 +2,12 @@ package security_in_action.ssia_ch5.config;
 
 import java.util.List;
 import javax.sql.DataSource;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -63,12 +61,5 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    public InitializingBean initializingBean() {
-        return () -> SecurityContextHolder.setStrategyName(
-                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL
-        );
     }
 }
--- ssia-ch5/src/main/java/security_in_action/ssia_ch5/controller/HelloController.java
+++ ssia-ch5/src/main/java/security_in_action/ssia_ch5/controller/HelloController.java
@@ -1,7 +1,13 @@
 package security_in_action.ssia_ch5.controller;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +26,21 @@ public class HelloController {
     @Async
     public void goodbye(Authentication a) {
         String username = a.getName();
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
     }
 }
