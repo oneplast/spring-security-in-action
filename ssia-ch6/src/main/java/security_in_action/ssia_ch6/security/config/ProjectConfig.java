c7c49b9e4ddd9fab513cc58f898ee823b1d51d3d
Gyeonghun Min <riverpower6@naver.com>
2025-09-05 17:41:23 +0900

    [FEAT] SecurityFilterChain 등록
    [DESCRIPTION]
    1. SecurityFilterChain 빈 등록
      - authenticationProviderService를 Provider로 설정
      - 모든 엔드포인트 인증 요구
      - 폼 로그인 방식 사용
      - 로그인 성공 시 /main 으로 리다이렉트

--- ssia-ch6/src/main/java/security_in_action/ssia_ch6/security/config/ProjectConfig.java
+++ ssia-ch6/src/main/java/security_in_action/ssia_ch6/security/config/ProjectConfig.java
@@ -1,12 +1,18 @@
 package security_in_action.ssia_ch6.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class ProjectConfig {
+
+    private final AuthenticationProviderService authenticationProvider;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -16,5 +22,17 @@ public class ProjectConfig {
     @Bean
     public SCryptPasswordEncoder sCryptPasswordEncoder() {
         return new SCryptPasswordEncoder(16384, 8, 1, 32, 64);  // 스프링부트 2 버전까지의 NoArgsConstructor 기본값
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authenticationProvider(authenticationProvider)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/main", true));
+
+        return http.build();
     }
 }
