2c792454a2474b168e83773abe56b31fc62fc36d
Gyeonghun Min <riverpower6@naver.com>
2025-09-05 18:48:47 +0900

    [FEAT] Lazy Loading 문제 해결
    [DESCRIPTION]
    1. 기존에 User를 가져올 때 authorities를 Lazy Loading으로 가져오도록 설정
    2. 해당 authorities를 hibernate 세션이 종료되기 전까지 사용하지 않아서, 실제 authorities 값이 아닌 프록시 객체가 그대로 잔존
    3. JOIN FETCH JPQL을 작성하여 해당 쿼리를 사용하면 그냥 한 번에 가져오도록 변경

--- ssia-ch6/src/main/java/security_in_action/ssia_ch6/security/repository/UserRepository.java
+++ ssia-ch6/src/main/java/security_in_action/ssia_ch6/security/repository/UserRepository.java
@@ -2,9 +2,12 @@ package security_in_action.ssia_ch6.security.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import security_in_action.ssia_ch6.security.model.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    Optional<User> findUserByUsername(String u);
+    @Query("SELECT u from User u JOIN FETCH u.authorities where u.username = :username")
+    Optional<User> findUserByUsername(@Param("username") String username);
 }
