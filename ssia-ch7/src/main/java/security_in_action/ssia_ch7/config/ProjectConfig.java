3dcbd94450c0fb9b00c1d4078bee6145dc0f951a
Gyeonghun Min <riverpower6@naver.com>
2025-09-06 14:12:33 +0900

    [FEAT] AuthorizeHttpRequestsConfigurer.access() 사용
    [DESCRIPTION]
    1. UserDetailsService 일부 변경
      - 읽기 권한을 가진 john, 읽기, 쓰기, 삭제 권한을 가진 jane을 등록
    2. .access() 사용
      - 스프링 부트 3버전부터 SpEL을 String으로 사용하는 방식이 Deprecated
      - 람다식을 이용해 직접 권한을 부여하는 방식 적용
      - 읽기 권한은 있고, 쓰기 권한은 없는 유저만 접근 허용

--- ssia-ch7/src/main/java/security_in_action/ssia_ch7/config/ProjectConfig.java
+++ ssia-ch7/src/main/java/security_in_action/ssia_ch7/config/ProjectConfig.java
@@ -1,9 +1,12 @@
 package security_in_action.ssia_ch7.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,7 +22,14 @@ public class ProjectConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().hasAuthority("WRITE"))
+                        .anyRequest().access((authentication, object) -> {
+                            List<String> authorities = authentication.get().getAuthorities().stream()
+                                    .map(GrantedAuthority::getAuthority)
+                                    .toList();
+
+                            boolean granted = authorities.contains("read") && !authorities.contains("delete");
+                            return new AuthorizationDecision(granted);
+                        }))
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
@@ -31,12 +41,12 @@ public class ProjectConfig {
 
         UserDetails user1 = User.withUsername("john")
                 .password("12345")
-                .authorities("READ")
+                .authorities("read")
                 .build();
 
         UserDetails user2 = User.withUsername("jane")
                 .password("12345")
-                .authorities("WRITE")
+                .authorities("read", "write", "delete")
                 .build();
 
         manager.createUser(user1);
