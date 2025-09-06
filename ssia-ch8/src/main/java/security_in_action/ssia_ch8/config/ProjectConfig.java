ecde695266797c3e3975c0bfdceed2befc81915e
Gyeonghun Min <riverpower6@naver.com>
2025-09-06 20:44:38 +0900

    [FEAT] HttpMethod 사용한 인증/인가 제한
    [DESCRIPTION]
    1. TestController 작성
      - HttpMethod가 POST인 엔드포인트도 함께 추가
    2. 같은 엔드포인트인 /a 에 대한 제한을 HttpMethod 별로 분리
      - HttpMethod.GET은 인증 필요
      - HttpMethod.Post는 모두 허가
    3. csrf 보호 비활성화
      - 테스트 용도이므로, csrf 보호는 비활성화
      - 스프링 부트 3 버전부터는 csrf(csrf -> csrf.disable()) 로 비활성화
        - 인스턴스 메서드 참조 방식으로 변경해서 사용

--- ssia-ch8/src/main/java/security_in_action/ssia_ch8/config/ProjectConfig.java
+++ ssia-ch8/src/main/java/security_in_action/ssia_ch8/config/ProjectConfig.java
@@ -2,8 +2,10 @@ package security_in_action.ssia_ch8.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,13 +21,30 @@ public class ProjectConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/hello").hasRole("ADMIN")
-                        .requestMatchers("/ciao").hasRole("MANAGER")
-                        .anyRequest().authenticated())
+                        .requestMatchers(HttpMethod.GET, "/a").authenticated()
+                        .requestMatchers(HttpMethod.POST, "/a").permitAll()
+                        .anyRequest().denyAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
+/*
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/hello").hasRole("ADMIN")
+                        .requestMatchers("/ciao").hasRole("MANAGER")
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+*/
 
     @Bean
     public UserDetailsService userDetailsService() {
