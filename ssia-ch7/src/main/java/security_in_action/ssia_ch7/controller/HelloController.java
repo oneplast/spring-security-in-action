342af44878c07fa0ffd6b54890919bab28977231
Gyeonghun Min <riverpower6@naver.com>
2025-09-06 13:44:39 +0900

    [FEAT] SecurityConfig 정의
    [DESCRIPTION]
    1. 테스트용 HelloController 생성
    2. UserDetailsService 빈으로 등록
      - InMemoryUserDetailsManager 사용
      - 읽기 권한 가진 john, 쓰기 권한 가진 jane을 등록
    3. PasswordEncoder 빈으로 등록
    4. httpBasic 방식을 사용하고, 모든 요청을 허용하는 SecurityFilterChain 등록

---
+++ ssia-ch7/src/main/java/security_in_action/ssia_ch7/controller/HelloController.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch7.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hell() {
+        return "Hello!";
+    }
+}
