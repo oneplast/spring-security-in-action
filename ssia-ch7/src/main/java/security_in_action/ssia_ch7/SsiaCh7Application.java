794b91abc29e185a0791209906d4c933ad920e17
Gyeonghun Min <riverpower6@naver.com>
2025-09-06 13:18:21 +0900

    [SETTING] Chapter7 프로젝트 세팅 및 README.md에 사용 버전 명시

---
+++ ssia-ch7/src/main/java/security_in_action/ssia_ch7/SsiaCh7Application.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh7Application.class, args);
+	}
+
+}
---
+++ ssia-ch7/src/test/java/security_in_action/ssia_ch7/SsiaCh7ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
