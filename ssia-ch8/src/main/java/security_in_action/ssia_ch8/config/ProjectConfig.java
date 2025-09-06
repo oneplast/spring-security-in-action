b14731790af9310ec803bb26a0098c1c709e10e1
Gyeonghun Min <riverpower6@naver.com>
2025-09-07 00:13:37 +0900

    [FEAT] 정규식 선택기로 정규식 매칭 엔드포인트에 보안 적용
    [DESCRIPTION]
    1. UserDetailsService 일부 수정
      - john에게 read 권한, jane에게 read, premium 권한 부여
    2. 정규식 선택기 사용하여 정규식 매칭 엔드포인트에 보안 적용
      - requestMatchers(new RegexRequestMatcher("정규식", httpMethod)).authenticated() 적용
    
    ======================================================================
    > 스프링 부트 3 버전 이상에서의 정규식 선택기 사용
    
    1. 이전 regexMatchers() 또한 스프링 3 버전부터 Deprecated
    2. requestMatchers는 Ant 스타일 매칭으로 처리되므로, 정규식 그대로 사용 불가
      - 인자로 RegexRequestMatcher("정규식", httpMethod) 인스턴스를 추가
      - httpMethod에 null을 넣으면 모든 HTTP 메서드에 대해 적용

--- ssia-ch8/src/main/java/security_in_action/ssia_ch8/config/ProjectConfig.java
+++ ssia-ch8/src/main/java/security_in_action/ssia_ch8/config/ProjectConfig.java
@@ -2,7 +2,6 @@ package security_in_action.ssia_ch8.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +12,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 @Configuration
 public class ProjectConfig {
@@ -21,8 +21,10 @@ public class ProjectConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/product/{code:^[0-9]+$}").permitAll()
-                        .anyRequest().denyAll()
+//                        .requestMatchers(HttpMethod.GET, "/product/{code:^[0-9]+$}").permitAll()
+//                        .anyRequest().denyAll()
+                                .requestMatchers(new RegexRequestMatcher(".*/(us|uk|ca)+/(en|fr).*", null)).authenticated()
+                                .anyRequest().hasAuthority("premium")
                 )
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
@@ -51,6 +53,27 @@ public class ProjectConfig {
 
         UserDetails user1 = User.withUsername("john")
                 .password("12345")
+                .authorities("read")
+                .build();
+
+        UserDetails user2 = User.withUsername("jane")
+                .password("12345")
+                .authorities("read", "premium")
+                .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+/*
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails user1 = User.withUsername("john")
+                .password("12345")
                 .roles("ADMIN")
                 .build();
 
@@ -64,6 +87,7 @@ public class ProjectConfig {
 
         return manager;
     }
+*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
