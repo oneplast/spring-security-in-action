d6fafdf9726bb8bba68e5ba2adbd668923b94d8d
Gyeonghun Min <riverpower6@naver.com>
2025-09-06 14:49:07 +0900

    [FEAT] 역할을 사용하여 인가 진행
    [DESCRIPTION]
    1. UserDetailsService 일부 변경
      - ROLE_ADMIN 이라는 역할을 john에게, ROLE_MANAGER 라는 역할을 jane에게 부여
      - .hasRole() 메서드를 사용하여 역할 검사
      - ADMIN 이라는 역할을 가진 유저만 접근 가능
        - hasRole()은 authroties에서 접두사 ROLE_을 제외하여 해당 역할 검사 실행

--- ssia-ch7/src/main/java/security_in_action/ssia_ch7/config/ProjectConfig.java
+++ ssia-ch7/src/main/java/security_in_action/ssia_ch7/config/ProjectConfig.java
@@ -1,12 +1,9 @@
 package security_in_action.ssia_ch7.config;
 
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,14 +19,7 @@ public class ProjectConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().access((authentication, object) -> {
-                            List<String> authorities = authentication.get().getAuthorities().stream()
-                                    .map(GrantedAuthority::getAuthority)
-                                    .toList();
-
-                            boolean granted = authorities.contains("read") && !authorities.contains("delete");
-                            return new AuthorizationDecision(granted);
-                        }))
+                        .anyRequest().hasRole("ADMIN"))
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
@@ -41,12 +31,12 @@ public class ProjectConfig {
 
         UserDetails user1 = User.withUsername("john")
                 .password("12345")
-                .authorities("read")
+                .authorities("ROLE_ADMIN")
                 .build();
 
         UserDetails user2 = User.withUsername("jane")
                 .password("12345")
-                .authorities("read", "write", "delete")
+                .authorities("ROLE_MANAGER")
                 .build();
 
         manager.createUser(user1);
