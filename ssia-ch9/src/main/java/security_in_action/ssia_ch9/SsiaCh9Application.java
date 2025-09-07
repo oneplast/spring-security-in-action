257e4eb92866055e0ef80aad8c86556e27add29f
Gyeonghun Min <riverpower6@naver.com>
2025-09-07 12:24:14 +0900

    [SETTING] Chapter9 프로젝트 세팅

---
+++ ssia-ch9/src/main/java/security_in_action/ssia_ch9/SsiaCh9Application.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh9Application.class, args);
+	}
+
+}
