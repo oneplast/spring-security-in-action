35532a152592bea0cf2bfe25da2ef717410f64a1
Gyeonghun Min <riverpower6@naver.com>
2025-09-08 09:10:24 +0900

    [SETTING] Chapter10 프로젝트 세팅

---
+++ ssia-ch10/src/test/java/security_in_action/ssia_ch10/SsiaCh10ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
