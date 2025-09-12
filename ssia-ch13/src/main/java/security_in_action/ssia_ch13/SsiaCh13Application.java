70fc5c28eeeed062d371a773a9f6aa5c7cb0595e
Gyeonghun Min <riverpower6@naver.com>
2025-09-12 09:52:14 +0900

    [SETTING] Chapter13 프로젝트 세팅
    [DESCRIPTION]
    1. 권한 부여 서버 의존성 다운로드 경로 변경
      - 책의 의존성 경로는 스프링 부트 3 버전에서 더이상 이용 불가
      - org.springframework.boot:spring-boot-starter-oauth2-authorization-server에서 의존성을 다운 받아서 동일한 방법으로 구현 가능

---
+++ ssia-ch13/src/main/java/security_in_action/ssia_ch13/SsiaCh13Application.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh13Application.class, args);
+	}
+
+}
---
+++ ssia-ch13/src/test/java/security_in_action/ssia_ch13/SsiaCh13ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
