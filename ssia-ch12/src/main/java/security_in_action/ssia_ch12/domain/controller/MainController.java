16cbd55a3a00892d877c285eae25edca997f7f72
Gyeonghun Min <riverpower6@naver.com>
2025-09-12 09:31:53 +0900

    [FEAT] OAuth2AuthenticationToken으로 유저 정보 확인
    [DESCRIPTION]
    1. OAuth2AuthenticationToken으로 유저 정보 확인
      - Authentication의 구현체인 OAuth2AuthenticationToken을 사용해 유저 정보 확인
      - SecurityContext에서 직접 가져오던가, 컨트롤러의 파라미터로 바로 꺼내올 수 있음

--- ssia-ch12/src/main/java/security_in_action/ssia_ch12/domain/controller/MainController.java
+++ ssia-ch12/src/main/java/security_in_action/ssia_ch12/domain/controller/MainController.java
@@ -1,13 +1,18 @@
 package security_in_action.ssia_ch12.domain.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Slf4j
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String main() {
+    public String main(OAuth2AuthenticationToken token) {
+        log.info(token.getPrincipal().toString());
+
         return "main.html";
     }
 }
