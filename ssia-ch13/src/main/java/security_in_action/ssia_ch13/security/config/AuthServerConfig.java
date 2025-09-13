b438c3b99ed0ff4f72076be4f655e5a87c64b917
Gyeonghun Min <riverpower6@naver.com>
2025-09-13 13:59:26 +0900

    [FEAT] 권한 승인 서버 - 승인 코드 그랜트 유형 구현
    [DESCRIPTION]
    1. 기존 사용했던 Token, Provider, Converter 주석 처리
    2. RegisteredClient 추가
      - GrantType에 AUTHORIZATION_CODE 부여
      - 스프링부트 3 부터 권한 부여 승인/거절 페이지 따로 구현 필요
        - 페이지를 따로 구현하는 건 굳이 필요 없다고 생각
        - clientSettings에서 requireAutorizationConsent(false)로 자동 승인 설정
      - redirectUri 설정
        - 권한 부여 서버가 인증 후 사용자를 리다이렉션 시켜줄 URI
        - 파라미터로 code 값 랜덤 부여
    3. OAuth2AuthorizationService 빈 등록
      - 실제 Authorization 객체를 저장하고 조회하는 전용 서비스
      - Authorization-Code, Access-Token, Refresh-Token, Scope, Client 등 데이터를 모두 관리하는 핵심 저장소 역할
        - /oauth2/authorize 호출 시 생성된 code를 저장
        - /oauth2/token 호출 시 함께 전달된 code 유효성 검사
      - InMemoryOAuth2AuthorizationService 구현체로 빈 등록
    4. OAuth2AuthorizationConsentService 빈 등록
      - 사용자가 클라이언트에게 권한 부여했는지 여부를 저장
        - 동의 여부를 저장하여, 한 번 동의하면 다음부터 동의 요청 진행하지 않음
      - InMemoryOAuth2AuthorizationConsentService 구현체로 빈 등록
    5. 두 SecurityFilterChain에 formLogin 방식 등록
    
    =====================================================================================
    > 두 SecurityFilterChain에 모두 formLogin 방식을 등록한 이유
    
    1. 두 필터 체인은 각각 상응하는 엔드포인트가 다름 (엔드포인트 분리)
    2. OAuth2 전용 필터 체인이 먼저 실행
      - formLogin 방식 검증을 통해 인증 진행
        - 성공 시 SecurityContext 저장
    3. 일반 필터 체인이 이후에 실행
      - redirect-uri인 /home 접근
      - OAuth2 필터 체인에서는 OAuth2 authorize 접근 시의 로그인 기능만 formLogin으로 지정 후 처리
        - 일반 /home 요청은 일반 필터 체인에서 실행
        - 여기서 formLogin을 지정하지 않으면, 로그인 처리 방법을 모르기 때문에 401 에러 발생
        - 즉, 일반 필터 체인에서는 OAuth2 체인의 인증을 인식하지 못함
      - 이러한 문제 해결을 위해 동일하게 formLogin 방식을 등록
    
    =======================================================================================
    > FormLogin()
    
    1. formLogin()은 사용자가 로그인 폼을 통해 인증할 수 있도록 Filter와 EntryPoint를 등록하는 역할
    2. 로그인 시 SecurityContext 세션 등록
    3. 만약 전에 해당 세션이 존재한다면 사용자가 또 로그인할 필요는 없음
    4. formLogin을 등록해 Filter와 EntryPoint만 지정해주면, 예제의 경우 이미 위에서 세션은 등록된 상태기 때문에 사용자 추가 로그인 필요 없음

--- ssia-ch13/src/main/java/security_in_action/ssia_ch13/security/config/AuthServerConfig.java
+++ ssia-ch13/src/main/java/security_in_action/ssia_ch13/security/config/AuthServerConfig.java
@@ -4,43 +4,39 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import security_in_action.ssia_ch13.security.oauth2.config.OAuth2PasswordAuthenticationConverter;
-import security_in_action.ssia_ch13.security.oauth2.config.OAuth2PasswordAuthenticationProvider;
-import security_in_action.ssia_ch13.security.properties.JwtProperty;
 
 @Configuration
 @RequiredArgsConstructor
 public class AuthServerConfig {
 
-    private final OAuth2PasswordAuthenticationConverter authPasswordConverter;
-    private final RegisteredClientRepository clientRepository;
-    private final JwtProperty jwtProperty;
+//    private final OAuth2PasswordAuthenticationConverter authPasswordConverter;
+//    private final RegisteredClientRepository clientRepository;
+//    private final JwtProperty jwtProperty;
 
     @Bean
     @Order(1)
     public SecurityFilterChain authServerFilterChain(HttpSecurity http, AuthenticationConfiguration authConfig)
             throws Exception {
-        AuthenticationManager manager = authConfig.getAuthenticationManager();
-        OAuth2AuthorizationServerConfigurer authConfigurer = oauth2AuthServerConfigurer()
-                .tokenEndpoint(token -> token
-                        .accessTokenRequestConverter(authPasswordConverter)
-                        .authenticationProvider(
-                                new OAuth2PasswordAuthenticationProvider(manager, clientRepository, jwtProperty)));
+//        AuthenticationManager manager = authConfig.getAuthenticationManager();
+        OAuth2AuthorizationServerConfigurer authConfigurer = oauth2AuthServerConfigurer();
+//                .tokenEndpoint(token -> token
+//                        .accessTokenRequestConverter(authPasswordConverter)
+//                        .authenticationProvider(
+//                                new OAuth2PasswordAuthenticationProvider(manager, clientRepository, jwtProperty)));
 
         http
                 .securityMatcher(authConfigurer.getEndpointsMatcher())
                 .with(authConfigurer, Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll());
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
@@ -48,10 +44,11 @@ public class AuthServerConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .anyRequest().authenticated());
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
 
         return http.build();
     }
--- ssia-ch13/src/main/java/security_in_action/ssia_ch13/security/oauth2/config/OAuth2Config.java
+++ ssia-ch13/src/main/java/security_in_action/ssia_ch13/security/oauth2/config/OAuth2Config.java
@@ -1,18 +1,25 @@
 package security_in_action.ssia_ch13.security.oauth2.config;
 
+import static org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE;
 import static org.springframework.security.oauth2.core.AuthorizationGrantType.PASSWORD;
 import static org.springframework.security.oauth2.core.AuthorizationGrantType.REFRESH_TOKEN;
 
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.InMemoryOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 
 @Configuration
 public class OAuth2Config {
 
+/*
     @Bean
     public RegisteredClientRepository clientRepository() {
         RegisteredClient client = RegisteredClient.withId("custom")
@@ -23,5 +30,42 @@ public class OAuth2Config {
                 .build();
 
         return new InMemoryRegisteredClientRepository(List.of(client));
+    }
+*/
+
+    @Bean
+    public RegisteredClientRepository clientRepository() {
+        RegisteredClient client1 = RegisteredClient.withId("custom1")
+                .clientId("client1")
+                .clientSecret("secret1")
+                .scopes(set -> set.addAll(List.of("read")))
+                .authorizationGrantTypes(set -> set.addAll(List.of(AUTHORIZATION_CODE)))
+                .redirectUri("http://localhost:8080/home")
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(false)
+                        .build())
+                .build();
+
+        RegisteredClient client2 = RegisteredClient.withId("custom2")
+                .clientId("client2")
+                .clientSecret("secret2")
+                .scopes(set -> set.addAll(List.of("read")))
+                .authorizationGrantTypes(set -> set.addAll(List.of(
+                        AUTHORIZATION_CODE, PASSWORD, REFRESH_TOKEN
+                )))
+                .redirectUri("http://localhost:8080/home")
+                .build();
+
+        return new InMemoryRegisteredClientRepository(List.of(client1, client2));
+    }
+
+    @Bean
+    public OAuth2AuthorizationService OAuth2AuthService() {
+        return new InMemoryOAuth2AuthorizationService();
+    }
+
+    @Bean
+    public OAuth2AuthorizationConsentService OAuth2ConsentService() {
+        return new InMemoryOAuth2AuthorizationConsentService();
     }
 }
