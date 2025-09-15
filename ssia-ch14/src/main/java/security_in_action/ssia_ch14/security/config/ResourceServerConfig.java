d5163144386c7ccf155cf44e8a7b375aabf03e3e
Gyeonghun Min <riverpower6@naver.com>
2025-09-15 16:39:06 +0900

    [FEAT] 리소스 서버에서 JWT 토큰 참조
    [DESCRIPTION]
    1. MySQL 의존성 설치
    2. spring.security.oauth2.resourceserver.jwt 프로퍼티 등록
      - jwk-set-uri: http://localhost:8080/oauth2/jwks
      - 권한 부여 서버 url 사용
    3. HttpSecurity 설정 변경
      - oauth2ResourceServer에서 opaquetoken 대신 jwt를 사용하도록 변경
    
    =======================================================================================
    > 스프링 부트 3 에서의 리소스 서버 DB 참조 - JWT 토큰 사용 시 매우 복잡
    
    1. 스프링 부트 2 까지 사용하던 TokenStore가 Deprecated
    2. HttpSecurity 설정의 oauth2ResourceServer 내부 설정 변경 필요
     ## 별다른 설정이 없었으면, 권한 부여 서버는 액세스 토큰을 JWT로 생성해서 부여
      - 기존에는 oauth -> oauth.opaqueToken 사용
        - 권한 부여 서버에서 받은 Access-Token을 /oauth2/introspect 엔드포인트 호출 시 헤더에 추가하여 인증 진행
        - DB 참조에서도 사용 가능하지만, JWT 토큰 파싱 문제 발생
      - 따라서, DB에 JWT를 저장한 점을 이용해서 JWT 방식을 적용
      - 권한 부여 서버에서 JWT 토큰을 사용한 경우, oauth -> oauth.jwt 설정으로 변경 필요
        - 설정 파일의 프로퍼티에 추가 uri 경로 등록 필요
        - spring.security.oauth2.resourceserver.jwt.jwk-set-uri: '권한 부여 서버 url'/oauth2/jwks
    
    - DB에 JWT가 저장된 경우, BLOB 타입의 JWT를 파싱하고 처리하는 게 어려움
    - 이를 해결하기 위해서는 코드가 매우 복잡해지므로, 필요한 경우 추가 구현 필요
    - JWT는 별다른 DB 없어도 유효기간 내에 사용 가능한 점을 이용하여, 리소스 서버는 jwt 참조 방식으로 변경
    - DB에 토큰들이 저장되기는 하므로, 후처리나 이슈 해결이 필요한 경우 이용해서 진행

--- ssia-ch14/src/main/java/security_in_action/ssia_ch14/security/config/ResourceServerConfig.java
+++ ssia-ch14/src/main/java/security_in_action/ssia_ch14/security/config/ResourceServerConfig.java
@@ -4,18 +4,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ResourceServerConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, OpaqueTokenIntrospector dbTokenIntrospector)
+            throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth -> oauth
-                        .opaqueToken(Customizer.withDefaults()));
+                        .jwt(Customizer.withDefaults()));
 
         return http.build();
     }
