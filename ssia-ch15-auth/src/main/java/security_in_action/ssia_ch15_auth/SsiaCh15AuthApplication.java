165abba35dabeb4872503150466ffb6412cd9cd5
Gyeonghun Min <riverpower6@naver.com>
2025-09-16 13:51:04 +0900

    [SETTING] Chapter15 프로젝트 세팅 - 권한 부여 서버

---
+++ ssia-ch15-auth/src/main/java/security_in_action/ssia_ch15_auth/SsiaCh15AuthApplication.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh15AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh15AuthApplication.class, args);
+	}
+
+}
---
+++ ssia-ch15-auth/src/test/java/security_in_action/ssia_ch15_auth/SsiaCh15AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh15AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
