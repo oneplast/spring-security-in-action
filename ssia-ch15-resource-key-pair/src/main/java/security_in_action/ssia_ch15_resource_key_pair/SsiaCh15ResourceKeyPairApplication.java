5a7c71d9306ffd68e7efce08dc73ab34fc9f7ed7
Gyeonghun Min <riverpower6@naver.com>
2025-09-17 13:45:38 +0900

    [SETTING] Chapter15 프로젝트 세팅 - 리소스 서버 (비대칭 키 사용)

---
+++ ssia-ch15-resource-key-pair/src/main/java/security_in_action/ssia_ch15_resource_key_pair/SsiaCh15ResourceKeyPairApplication.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_resource_key_pair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh15ResourceKeyPairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh15ResourceKeyPairApplication.class, args);
+	}
+
+}
---
+++ ssia-ch15-resource-key-pair/src/test/java/security_in_action/ssia_ch15_resource_key_pair/SsiaCh15ResourceKeyPairApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_resource_key_pair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh15ResourceKeyPairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
