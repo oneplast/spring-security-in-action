3f14e08e838cf1b2a8a36ad18877ef37d7a48b3e
Gyeonghun Min <riverpower6@naver.com>
2025-09-17 12:09:34 +0900

    [SETTING] Chapter15 프로젝트 세팅 - 권한 부여 서버 (비대칭 키 사용)

---
+++ ssia-ch15-auth-key-pair/src/main/java/security_in_action/ssia_ch15_auth_key_pair/SsiaCh15AuthKeyPairApplication.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_auth_key_pair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh15AuthKeyPairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh15AuthKeyPairApplication.class, args);
+	}
+
+}
---
+++ ssia-ch15-auth-key-pair/src/test/java/security_in_action/ssia_ch15_auth_key_pair/SsiaCh15AuthKeyPairApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_auth_key_pair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh15AuthKeyPairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
