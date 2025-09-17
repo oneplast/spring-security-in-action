44cc2d382ce52216f08a81e9bbe0f7725afc5a70
Gyeonghun Min <riverpower6@naver.com>
2025-09-17 10:24:47 +0900

    [SETTING] Chapter15 프로젝트 세팅 - 리소스 서버

---
+++ ssia-ch15-resource/src/main/java/security_in_action/ssia_ch15_resource/SsiaCh15ResourceApplication.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh15ResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh15ResourceApplication.class, args);
+	}
+
+}
---
+++ ssia-ch15-resource/src/test/java/security_in_action/ssia_ch15_resource/SsiaCh15ResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch15_resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh15ResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
