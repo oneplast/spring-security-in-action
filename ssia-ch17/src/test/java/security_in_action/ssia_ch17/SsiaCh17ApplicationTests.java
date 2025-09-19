20425c2a65a657f0a649e5c5a65df44a8a2c1231
Gyeonghun Min <riverpower6@naver.com>
2025-09-19 21:45:07 +0900

    [SETTING] Chapter17 프로젝트 세팅

---
+++ ssia-ch17/src/test/java/security_in_action/ssia_ch17/SsiaCh17ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
