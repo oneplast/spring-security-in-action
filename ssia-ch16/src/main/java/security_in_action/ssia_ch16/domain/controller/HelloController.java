a1e6c1c5f6b5f078188d0b746c291c8ece9922c0
Gyeonghun Min <riverpower6@naver.com>
2025-09-19 18:11:35 +0900

    [FEAT] 메서드 매개 변수 값으로 사전 권한 부여
    [DESCRIPTION]
    1. 사용자별 비밀 이름 Map으로 등록
    2. SpEL 사용하여, 매개변수 이름과 인증 이름이 같은 경우만 메서드 실행
      - #name == authentication.principal.username

--- ssia-ch16/src/main/java/security_in_action/ssia_ch16/domain/controller/HelloController.java
+++ ssia-ch16/src/main/java/security_in_action/ssia_ch16/domain/controller/HelloController.java
@@ -1,7 +1,9 @@
 package security_in_action.ssia_ch16.domain.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import security_in_action.ssia_ch16.domain.service.NameService;
 
@@ -14,5 +16,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, " + nameService.getName();
+    }
+
+    @GetMapping("/secret/names/{name}")
+    public List<String> names(@PathVariable String name) {
+        return nameService.getSecretNames(name);
     }
 }
--- ssia-ch16/src/main/java/security_in_action/ssia_ch16/domain/service/NameService.java
+++ ssia-ch16/src/main/java/security_in_action/ssia_ch16/domain/service/NameService.java
@@ -1,13 +1,25 @@
 package security_in_action.ssia_ch16.domain.service;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NameService {
 
+    private Map<String, List<String>> secretNames = Map.of(
+            "natalie", List.of("Energico", "Perfecto"),
+            "emma", List.of("Fantastico")
+    );
+
     @PreAuthorize("hasAuthority('write')")
     public String getName() {
         return "Fantastico";
+    }
+
+    @PreAuthorize("#name == authentication.principal.username")
+    public List<String> getSecretNames(String name) {
+        return secretNames.get(name);
     }
 }
