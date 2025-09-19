db94f462f7543ac1def7141f2a23ac1221e5ca88
Gyeonghun Min <riverpower6@naver.com>
2025-09-19 21:25:45 +0900

    [FEAT] 메서드 사용 권한 구현 - PermissionEvaluator 인자 3개 사용
    [DESCRIPTION]
    1. 파라미터 code를 id로 사용
    2. 타입은 임의 지정
      - 예제에서는 document
    3. permission에 권한 지정

--- ssia-ch16/src/main/java/security_in_action/ssia_ch16/domain/service/DocumentService.java
+++ ssia-ch16/src/main/java/security_in_action/ssia_ch16/domain/service/DocumentService.java
@@ -1,7 +1,7 @@
 package security_in_action.ssia_ch16.domain.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import security_in_action.ssia_ch16.domain.entity.Document;
 import security_in_action.ssia_ch16.domain.repository.DocumentRepository;
@@ -12,7 +12,8 @@ public class DocumentService {
 
     private final DocumentRepository documentRepository;
 
-    @PostAuthorize("hasPermission(returnObject, 'ROLE_admin')")
+    //    @PostAuthorize("hasPermission(returnObject, 'ROLE_admin')")
+    @PreAuthorize("hasPermission(#code, 'document', 'ROLE_admin')")
     public Document getDocument(String code) {
         return documentRepository.findDocument(code);
     }
--- ssia-ch16/src/main/java/security_in_action/ssia_ch16/security/config/DocumentPermissionEvaluator.java
+++ ssia-ch16/src/main/java/security_in_action/ssia_ch16/security/config/DocumentPermissionEvaluator.java
@@ -1,16 +1,22 @@
 package security_in_action.ssia_ch16.security.config;
 
 import java.io.Serializable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import security_in_action.ssia_ch16.domain.entity.Document;
+import security_in_action.ssia_ch16.domain.repository.DocumentRepository;
 
 @Component
+@RequiredArgsConstructor
 public class DocumentPermissionEvaluator implements PermissionEvaluator {
+
+    private final DocumentRepository documentRepository;
 
     @Override
     public boolean hasPermission(Authentication a, Object subject, Object permission) {
+/*
         Document document = (Document) subject;
         String p = (String) permission;
 
@@ -18,10 +24,20 @@ public class DocumentPermissionEvaluator implements PermissionEvaluator {
                 .anyMatch(auth -> auth.getAuthority().equals(p));
 
         return admin || document.getOwner().equals(a.getName());
+*/
+        return false;
     }
 
     @Override
     public boolean hasPermission(Authentication a, Serializable id, String type, Object permission) {
-        return false;
+        String code = id.toString();
+        Document document = documentRepository.findDocument(code);
+
+        String p = permission.toString();
+
+        boolean admin = a.getAuthorities().stream()
+                .anyMatch(auth -> auth.getAuthority().equals(p));
+
+        return admin || document.getOwner().equals(a.getName());
     }
 }
