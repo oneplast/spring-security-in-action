5a1f35527d3303976c0b0179d427a142bb23874c
Gyeonghun Min <riverpower6@naver.com>
2025-09-19 13:41:44 +0900

    [SETTING] Chapter16 프로젝트 세팅

---
+++ ssia-ch16/src/main/java/security_in_action/ssia_ch16/SsiaCh16Application.java
@@ -0,0 +1,15 @@
+package security_in_action.ssia_ch16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@SpringBootApplication
+@EnableMethodSecurity(prePostEnabled = true)
+public class SsiaCh16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh16Application.class, args);
+    }
+
+}
---
+++ ssia-ch16/src/test/java/security_in_action/ssia_ch16/SsiaCh16ApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
