87b15c2918ab27645c07430f90dcd72221f6e1c6
Gyeonghun Min <riverpower6@naver.com>
2025-09-19 09:39:59 +0900

    [FEAT] 리소스 서버 JWT 토큰 사용 - 비대칭 키, 공개 키를 노출하는 엔드포인트 이용
    [DESCRIPTION]
    1. 기존 사용하던 JwtDecoder, JwtAuthenticationConverter DI 주석 처리
      - 해당 구현체들은 yml 설정 파일의 프로퍼티를 직접 파싱하고 인증에 사용
      - 엔드포인트를 사용하면 이런 복잡한 절차 필요 없이 yml 설정 파일에 엔드포인트를 프로퍼티로 지정
      - 비대칭 키는 공개 키를 외부에 노출하기 때문에, 엔드포인트를 이용하여 공개 키 접근 가능
        - 공개 키 노출은 JWKS(JSON Web Key Set) 라는 표준 포맷에 따라 이뤄짐
    2. yml 설정에 엔드포인트 프로퍼티 등록
      - spring.security.oauth2.resourceserver.jwt.jwk-set-uri: {domain}/oauth2/jwks
        - {domain}에는 권한 부여 서버 도메인 입력
      - jwk-set-uri를 통해 공개 키들을 캐싱
      - 이후, JWT의 kid(key ID), alg(알고리즘) 헤더를 기준으로, 적절한 키를 찾아 서명 검증에 사용

--- ssia-ch15-resource-key-pair/src/main/java/security_in_action/ssia_ch15_resource_key_pair/security/config/ResourceServerConfig.java
+++ ssia-ch15-resource-key-pair/src/main/java/security_in_action/ssia_ch15_resource_key_pair/security/config/ResourceServerConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,9 +12,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class ResourceServerConfig {
 
-    private final JwtDecoder jwtDecoder;
+    //    private final JwtDecoder jwtDecoder;
     private final JwtAuthenticationConverter jwtAuthConverter;
 
+/*
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http
@@ -25,6 +25,20 @@ public class ResourceServerConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(token -> token
                                 .decoder(jwtDecoder)
+                                .jwtAuthenticationConverter(jwtAuthConverter)));
+
+        return http.build();
+    }
+*/
+
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
                                 .jwtAuthenticationConverter(jwtAuthConverter)));
 
         return http.build();
