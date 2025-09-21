476be02eeda1d07851b7676e805463ad62cc3ed7
Gyeonghun Min <riverpower6@naver.com>
2025-09-21 21:35:04 +0900

    [SETTING] Chapter19 프로젝트 세팅

---
+++ ssia-ch19/src/main/java/security_in_action/ssia_ch19/SsiaCh19Application.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh19Application.class, args);
+	}
+
+}
