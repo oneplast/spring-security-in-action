a3ff2a7510c7cb97482d3001248bfa74a92fcf4a
Gyeonghun Min <riverpower6@naver.com>
2025-09-22 16:45:53 +0900

    [TEST] `@WithMockUser` 사용하여 임의 유저 인증
    [DESCRIPTION]
    1. `@WithMockUser` 어노테이션으로 임의의 유저로 인증
      - 어노테이션 내부를 살펴보면, value = user
      - WithMockUserSecurityContextFactory를 확인해보면, username이 비었으면 value를 username으로 사용
      - principal.name이 user로 등록

--- ssia-ch20/src/test/java/security_in_action/ssia_ch20/MainTests.java
+++ ssia-ch20/src/test/java/security_in_action/ssia_ch20/MainTests.java
@@ -1,12 +1,14 @@
 package security_in_action.ssia_ch20;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -20,5 +22,13 @@ public class MainTests {
     public void helloUnauthenticated() throws Exception {
         mvc.perform(get("/hello"))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser   // 인증된 사용자 사용 (Annotation 내부를 보면 username을 user로 등록, username이 없으면 username = value)
+    public void helloAuthenticated() throws Exception {
+        mvc.perform(get("/hello"))
+                .andExpect(content().string("Hello!"))
+                .andExpect(status().isOk());
     }
 }
