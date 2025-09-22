206372fa17b7d47543e2f5349d081ac63e0d7b06
Gyeonghun Min <riverpower6@naver.com>
2025-09-22 13:18:51 +0900

    [FEAT] 리액티브 메서드 보안 - 사전 권한 부여 적용

--- ssia-ch19-method/src/main/java/security_in_action/ssia_ch19_method/SsiaCh19MethodApplication.java
+++ ssia-ch19-method/src/main/java/security_in_action/ssia_ch19_method/SsiaCh19MethodApplication.java
@@ -2,12 +2,14 @@ package security_in_action.ssia_ch19_method;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 
 @SpringBootApplication
+@EnableReactiveMethodSecurity
 public class SsiaCh19MethodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SsiaCh19MethodApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh19MethodApplication.class, args);
+    }
 
 }
