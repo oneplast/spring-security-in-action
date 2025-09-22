475f0ef790be8050eeb17a7976d1429e25108525
Gyeonghun Min <riverpower6@naver.com>
2025-09-22 13:11:54 +0900

    [SETTING] Chapter19 프로젝트 세팅 - 리액티브 메서드 보안

---
+++ ssia-ch19-method/src/main/java/security_in_action/ssia_ch19_method/SsiaCh19MethodApplication.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch19_method;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh19MethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh19MethodApplication.class, args);
+	}
+
+}
---
+++ ssia-ch19-method/src/test/java/security_in_action/ssia_ch19_method/SsiaCh19MethodApplicationTests.java
@@ -0,0 +1,13 @@
+package security_in_action.ssia_ch19_method;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh19MethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
