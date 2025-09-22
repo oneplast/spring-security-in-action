b3843cb5c872c2e41beb1395f56648c3cdce252c
Gyeonghun Min <riverpower6@naver.com>
2025-09-22 11:21:20 +0900

    [FEAT] Authentication을 Mono로 받아서 사용
    [DESCRIPTION]
    1. HelloController 수정
      - 엔드포인트 요청 시 Mono<Authentication> 요청 추가
      - 요청에 들어온 Authentication을 이용해 메시지 반환

--- ssia-ch19/src/main/java/security_in_action/ssia_ch19/domain/controller/HelloController.java
+++ ssia-ch19/src/main/java/security_in_action/ssia_ch19/domain/controller/HelloController.java
@@ -1,5 +1,6 @@
 package security_in_action.ssia_ch19.domain.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -8,7 +9,9 @@ import reactor.core.publisher.Mono;
 public class HelloController {
 
     @GetMapping("/hello")
-    public Mono<String> hello() {
-        return Mono.just("Hello!");
+    public Mono<String> hello(Mono<Authentication> auth) {
+        Mono<String> message = auth.map(a -> "Hello " + a.getName());
+
+        return message;
     }
 }
