aa6695657dc51cbaa9e2d90173b479fe3f7fd81b
Gyeonghun Min <riverpower6@naver.com>
2025-09-22 11:25:53 +0900

    [REFACTOR] ReactiveSecurityContextHolder를 통한 인증 객체 사용

--- ssia-ch19/src/main/java/security_in_action/ssia_ch19/domain/controller/HelloController.java
+++ ssia-ch19/src/main/java/security_in_action/ssia_ch19/domain/controller/HelloController.java
@@ -1,6 +1,7 @@
 package security_in_action.ssia_ch19.domain.controller;
 
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,8 +10,10 @@ import reactor.core.publisher.Mono;
 public class HelloController {
 
     @GetMapping("/hello")
-    public Mono<String> hello(Mono<Authentication> auth) {
-        Mono<String> message = auth.map(a -> "Hello " + a.getName());
+    public Mono<String> hello() {
+        Mono<String> message = ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(auth -> "Hello " + auth.getName());
 
         return message;
     }
