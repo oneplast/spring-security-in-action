e2541d4d4b90469d8099055c7fcb30d066e6e33c
Gyeonghun Min <riverpower6@naver.com>
2025-09-23 17:24:41 +0900

    [TEST] 리액티브 스프링 시큐리티 테스트 - mockUser() 사용
    [DESCRIPTION]
    1. client.mutateWith(mockUser())를 사용하여 리액티브 환경 인증 처리
      - mockUser 파라미터에서 username 설정 가능
      - .roles 파라미터에서 역할 부여 가능
      - 현재 ReactiveAuthorizationManager에서 ADMIN 역할 소유자만 접근 가능하도록 설정해두었으므로, ADMIN 역할 임의 부여

--- ssia-ch19/src/test/java/security_in_action/ssia_ch19/MainTests.java
+++ ssia-ch19/src/test/java/security_in_action/ssia_ch19/MainTests.java
@@ -1,5 +1,7 @@
 package security_in_action.ssia_ch19;
 
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockUser;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -18,6 +20,15 @@ class MainTests {
         client.get()
                 .uri("/hello")
                 .headers(headers -> headers.setBasicAuth("john", "12345"))
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    void testCallHelloWithValidUserWithMutate() {
+        client.mutateWith(mockUser().roles("ADMIN"))   // 리액티브 환경 인증 처리, ADMIN 역할 부여
+                .get()
+                .uri("/hello")
                 .exchange()
                 .expectStatus().isOk();
     }
