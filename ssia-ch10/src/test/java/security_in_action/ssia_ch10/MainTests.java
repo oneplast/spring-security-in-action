ced0b57c6f56913594d88a6783192c77e813402c
Gyeonghun Min <riverpower6@naver.com>
2025-09-23 16:56:26 +0900

    [TEST] CORS 구성 테스트
    [DESCRIPTION]
    1. Http Options 방식으로 Preflight 진행
      - OPTIONS 요청
      - 브라우저가 POST 메서드를 보낼 수 있는지 확인
    2. 허용되지 않은 Origin은 403 발생하는지 확인
      - https://www.example.com Origin으로 요청
      - CorsConfigurationSource에서 허용하지 않은 Origin
    3. 허용된 Origin은 접근 가능한지 확인
      - https://127.0.0.1:8080 Origin으로 요청
      - 결과들 중 CORS 관련 헤더들이 제대로 구성되었는지 확인

--- ssia-ch10/src/test/java/security_in_action/ssia_ch10/MainTests.java
+++ ssia-ch10/src/test/java/security_in_action/ssia_ch10/MainTests.java
@@ -1,7 +1,9 @@
 package security_in_action.ssia_ch10;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
@@ -26,6 +28,26 @@ class MainTests {
     @Test
     public void testHelloPOSTWithCSRF() throws Exception {
         mvc.perform(post("/hello").with(csrf()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testCORSFailed() throws Exception {
+        mvc.perform(options("/test")
+                        .header("Access-Control-Request-Method", "POST")
+                        .header("Origin", "https://www.example.com"))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void testCORSSuccess() throws Exception {
+        mvc.perform(options("/test")
+                        .header("Access-Control-Request-Method", "POST")
+                        .header("Origin", "https://127.0.0.1:8080"))
+                .andExpect(header().exists("Access-Control-Allow-Origin"))
+                .andExpect(header().string("Access-Control-Allow-Origin", "https://127.0.0.1:8080"))
+                .andExpect(header().exists("Access-Control-Allow-Methods"))
+                .andExpect(header().string("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE"))
                 .andExpect(status().isOk());
     }
 }
